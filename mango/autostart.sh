00a9317253429dae13ecb8601edf44a25a3ec037
Christian Bendiksen <christian@bendiksen.me>
2026-03-13 21:55:06 +0100

    add mango

---
+++ mango/autostart.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
+eval "$(dbus-launch --sh-syntax)" &
+
+# fast launch on GTK/Qt apps
+fc-cache -f &
+gtk-update-icon-cache -q &
+
+# polkit (auth)
+if ! pgrep -x "xfce-polkit" >/dev/null; then
+  /usr/lib/xfce-polkit/xfce-polkit &
+fi
+
+# Waybar and launcher
+awww-daemon &
+waybar &
+elephant &
+walker --gapplication-service &
