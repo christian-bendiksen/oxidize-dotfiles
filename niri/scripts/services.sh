c462f0c2c5f007adb09fe14430feeed4484b19dd
Christian Bendiksen <christian@bendiksen.me>
2026-04-29 21:41:18 +0200

    niri/hypr: menu binds

--- niri/scripts/services.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-awww-daemon &
-swayosd-server &
-elephant &
-walker --gapplication-service &
-until awww query 2>/dev/null; do sleep 0.2; done
-awww img ~/.config/oxidize/themes/background --transition-type=none
